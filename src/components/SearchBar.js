8277654496403da2380460d03e910aab3af57aa0
shazy89 <erdo.shazy123@gmail.com>
2020-12-21 16:47:32 -0500

    search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = () => {
 
    return (
        <View style={styles.background}>
-            <Feather name="search" size={24} color="black" /> 
+            <Feather name="search" style={styles.iconStyle} color="black" /> 
             <TextInput style={styles.inputStyle}
             placeholder='Search' 
             />
@@ -20,17 +20,21 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
     background: {
-        
-        backgroundColor: '#F0EEEE',
+        marginTop: 12,
+        backgroundColor: '#ededed',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-        alignItems: 'center',
-        flexDirection: 'row' 
+        flexDirection: 'row' ,
+        
     }, 
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1 
+        flex: 1,
+        fontSize: 18 
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center'
     } 
 })
 
